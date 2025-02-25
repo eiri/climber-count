c065ca34aa45270afe687a3d78dd7211cb097f8d
Eric Avdey <eiri@eiri.ca>
2025-02-24 22:33:04 -0400

    fix: use new go-quartz initializers

--- logger.go
+++ logger.go
@@ -3,13 +3,9 @@ package main
 import (
 	"log/slog"
 	"os"
-
-	qlog "github.com/reugn/go-quartz/logger"
 )
 
 func SetLogger() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
-	// turn off quarz logger
-	qlog.SetDefault(qlog.NewSimpleLogger(nil, qlog.LevelOff))
 }
